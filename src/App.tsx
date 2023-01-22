9152f2cfd5f43af1b3a5ea27a137878e13a6b063
Maarten Van Steenkiste <maarten.vansteenkiste@vlaanderen.be>
2023-01-22 12:31:28 +0100

    🗑️ Clean up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 import Board from "./Board";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default () => {
   return (
     <div className="App">
       <Board />
     </div>
   );
-}
-
-export default App;
+};
